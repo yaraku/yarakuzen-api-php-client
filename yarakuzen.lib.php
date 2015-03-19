72ec6c9da41ed78c0819e931d2238efdc6dac368
Marcelo Coraça de Freitas <marcelo@yaraku.com>
2015-03-19 20:24:55 +0900

    ZEN-3302 #comment YarakuZenApi inside a namespace and using objetcts to construct the method call

--- yarakuzen.lib.php
+++ yarakuzen.lib.php
@@ -12,9 +12,11 @@
  * @link      http://www.yarakuzen.com
  */
 
+namespace YarakuZenApi;
+
 /**
  * Class DocBlock
- * YarakuZenClient
+ * Client
  *
  * @category YarakuZen
  * @package  ClientLibrary
@@ -23,10 +25,12 @@
  * @license  http://www.yaraku.co.jp Proprietary
  * @link     http://www.yarakuzen.com
  */
-class YarakuZenClient{
+class Client{
 
-	private $key;
-	private $secret;
+	const HTTP_POST = 1;
+
+	private $publicKey;
+	private $privateKey;
 
 
 	/////////////////////////////////////////////////////////
@@ -43,6 +47,9 @@ class YarakuZenClient{
 	protected $_httpPass;
 
 
+	protected $_userAgent = "YarakuZen PHP Client v1.0";
+	protected $_referer = "";
+
 
 	/**
 	 * When using HTTPS disable the certificate validation
@@ -54,21 +61,21 @@ class YarakuZenClient{
 	protected $_target = "en";
 
 
-	const 
-		HTTP_GET = 1,
-		HTTP_POST = 2,
-		HTTP_PUT = 3;
+	/**
+	 * The charset for the request
+	 */
+	protected $_charset = "UTF-8";
+
 
 	/**
 	 * Initializes the client using the given API Key and Secret generated at Yaraku.
 	 *
-	 * @param key the API key
-	 * @param secret the API secret
+	 * @param publicKey the API publicKey
+	 * @param privateKey the API privateKey
 	 */
-	function __construct($key, $secret){
-		$this->key = $key;
-		$this->secret = $secret;
-		$this->url = $url;
+	function __construct($publicKey, $privateKey){
+		$this->publicKey = $publicKey;
+		$this->privateKey = $privateKey;
 	}
 
 	////////////////////////////
@@ -99,13 +106,13 @@ class YarakuZenClient{
 	 * Disables the certificate validation for HTTPS requests if parameter is unset or true.
 	 * @return $this
 	 */
-	public function insecure($insecure){
+	public function insecure($insecure=true){
 		$this->_insecure = !isset($insecure) || $insecure == true;
 		return $this;
 	}
 
 	/**
-	 * Use HTTP autentication on top of key/secret auth
+	 * Use HTTP autentication on top of publicKey/privateKey auth
 	 *
 	 * @param $username the username for HTTP auth
 	 * @param $password the password for HTTP auth
@@ -117,9 +124,46 @@ class YarakuZenClient{
 		return $this;
 	}
 
-	////////////////////
+	/**
+	 * Sets the userAgent informed to the server
+	 *
+	 * @param $userAgent should be a string that identifies your application to the server.
+	 * @return $this
+	 */
+	public function userAgent($userAgent){
+		$this->_userAgent = $userAgent;
+		return $this;
+	}
+
+	/**
+	 * Set the referer informed to the server.
+	 *
+	 * @param $referer the referer
+	 * @return $this
+	 */
+	public function referer($referer){
+		$this->_referer = $referer;
+		return $this;
+	}
+
+	/**
+	 * Sets the charset for this request.
+	 *
+	 * @param $charset the charset of the request
+	 * @return $this
+	 */
+	public function charset($charset){
+		$this->_charset = $charset;
+		return $this;
+	}
+
+	//////////////////////
 	// Text API Method //
 	////////////////////
+
+	public function callTexts($payload){
+		return $this->__callApi(Client::HTTP_POST, "texts", $payload);
+	}
 
 	////////////////////
 	// Inner Working //
@@ -129,16 +173,18 @@ class YarakuZenClient{
 	 * Sign the given payload, returning it.
 	 */
 	protected function __sign($payload){
-		$payload->publicKey = $this->key;
+		$payload->publicKey = $this->publicKey;
 		$payload->timestamp = time(); // A Unix timestamp
-		$payload->signature = hash_hmac('sha1', $payload->timestamp.$this->key, $this->secret); // Create a sha1 hash
+		$payload->signature = hash_hmac('sha1', $payload->timestamp.$this->publicKey, $this->privateKey); // Create a sha1 hash
+
+		return $payload;
 	}
 
 	/**
 	 * Return the given payload as a JSON encoded string of an object.
 	 * Make sure it's actually signed before returning.
 	 */
-	protected function __toJson($payload){
+	protected function __preparePayload($payload){
 		if(is_object($payload))
 			$obj = clone $payload;
 		elseif($is_array($payload))
@@ -146,7 +192,9 @@ class YarakuZenClient{
 		else
 			$obj = (object) array();
 
-		return $this->__sign($obj);
+
+		$o = $this->__sign($obj);
+		return http_build_query($o);
 	}
 
 	/**
@@ -155,7 +203,9 @@ class YarakuZenClient{
 	protected function __callApi($httpMethod, $apiMethod, $payload){
 
 		$curl = curl_init();
-		$pl = $this->__toJson($payload);
+		$pl = $this->__preparePayload($payload);
+
+
 
 		curl_setopt($curl, CURLOPT_URL, $this->_url."/".$apiMethod);
 
@@ -168,28 +218,102 @@ class YarakuZenClient{
 			curl_setopt($curl, CURLOPT_USERPWD, $this->_httpUser.':'.$this->_httpPass);
 
 		switch($httpMethod){
-			case YarakuClient::HTTP_GET:
-				curl_setopt($curl,CURLOPT_GET, true); // just being verbose.. didn't really need it
-			case YarakuClient::HTTP_POST:
+			case Client::HTTP_POST:
 				curl_setopt($curl,CURLOPT_POST, true);
 				curl_setopt($curl,CURLOPT_POSTFIELDS, $pl);
 				break;
 			default:
+				throw new Exception("HTTP Method Not Supported");
 		}
 
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, !$this->_insecure);
-		curl_setopt($curl, CURLOPT_USERAGENT, $this->_useragent);
+		curl_setopt($curl, CURLOPT_USERAGENT, $this->_userAgent);
 		curl_setopt($curl, CURLOPT_REFERER, $this->_referer);
+
+
+		$contentType = 'application/x-www-form-urlencoded;charset='.$this->_charset;
+		curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-type: $contentType"]);
 
 		$this->_response = curl_exec($curl);
 		$this->_status = curl_getinfo($curl,CURLINFO_HTTP_CODE);
 
 		curl_close($curl); 
 
+
 		// TODO: treat the response
 		return json_decode($this->_response);
 	}
 }
+
+/**
+ * Class DocBlock
+ * TextData
+ *
+ * @category YarakuZen
+ * @package  ClientLibrary
+ * @author   Marcelo C. de Freitas <marcelo@yaraku.com>
+ * @copyright 2015 Yaraku, Inc.
+ * @license  http://www.yaraku.co.jp Proprietary
+ * @link     http://www.yarakuzen.com
+ */
+class TextData{
+	public function _customData($customData){
+		$this->customData = $customData;
+		return $this;
+	}
+
+	public function _text($text){
+		$this->text = $text;
+		return $this;
+	}
+
+	public function _machineTranslate($machineTranslate = 1){
+		$this->machineTranslate = $machineTranslate != false;
+		return $this;
+	}
+}
+
+/**
+ * Class DocBlock
+ * RequestPayload
+ *
+ * @category YarakuZen
+ * @package  ClientLibrary
+ * @author   Marcelo C. de Freitas <marcelo@yaraku.com>
+ * @copyright 2015 Yaraku, Inc.
+ * @license  http://www.yaraku.co.jp Proprietary
+ * @link     http://www.yarakuzen.com
+ */
+class RequestPayload{
+
+	public function _lcSrc($source){
+		$this->lcSrc = $source;
+		return $this;
+	}
+
+	public function _lcTgt($target){
+		$this->lcTgt = $target;
+		return $this;
+	}
+
+	public function _machineTranslate($machineTranslate = true){
+		$this->machineTranslate = $machineTranslate != false;
+		return $this;
+	}
+
+	public function _persist($persist = true){
+		$this->persist = $persist;
+		return $this;
+	}
+
+	public function addText($text){
+		if(!isset($this->texts))
+			$this->texts = array();
+		$this->texts[] = $text;
+		return $this;
+	}
+}
+
 
 
 ?>
