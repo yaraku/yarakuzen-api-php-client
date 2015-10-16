219603e75d35ccb9db784bf2fe4e7c835d032d45
Jonas <jonas@yaraku.com>
2015-10-16 09:56:10 +0900

    Adapts to the PSR2 code style

--- yarakuzen.lib.php
+++ yarakuzen.lib.php
@@ -1,6 +1,7 @@
-<?php
+<?php namespace YarakuZenApi;
+
 /**
- * Client library for the Yaraku REST API. 
+ * Client library for the Yaraku REST API.
  *
  * PHP Version 5
  *
@@ -12,8 +13,7 @@
  * @link      http://www.yarakuzen.com
  */
 
-namespace YarakuZenApi;
-
+use Exception;
 
 /**
  * Class DocBlock
@@ -28,30 +28,41 @@ namespace YarakuZenApi;
  * @license  MIT
  * @link     http://www.yarakuzen.com
  */
-abstract class Tier{
-    public abstract function toString();
+abstract class Tier
+{
+    abstract public function toString();
 }
 
-class CasualTier extends Tier{
-    public function toString(){ return "casual"; }
+class CasualTier extends Tier
+{
+    public function toString()
+    {
+        return "casual";
+    }
 }
 
-class StandardTier extends Tier{
-    public function toString(){ return "standard"; }
+class StandardTier extends Tier
+{
+    public function toString()
+    {
+        return "standard";
+    }
 }
 
-class BusinessTier extends Tier{
-    public function toString(){ return "business"; }
+class BusinessTier extends Tier
+{
+    public function toString()
+    {
+        return "business";
+    }
 }
-
-
 
 
 /**
  * Class DocBlock
  * TextData
  *
- * A text block that will be sent to the YarakZen API.
+ * A text block that will be sent to the YarakuZen API.
  *
  * Each method actually sets the variable for the same name.
  *
@@ -64,21 +75,34 @@ class BusinessTier extends Tier{
  * @license  MIT
  * @link     http://www.yarakuzen.com
  */
-class TextData{
-	public function customData($customData){
-		$this->customData = $customData;
-		return $this;
-	}
+class TextData
+{
+    /** @var string */
+    public $customData;
 
-	public function text($text){
-		$this->text = $text;
-		return $this;
-	}
+    /** @var string */
+    public $text;
 
-	public function textFromFile($fileName){
-		$this->text = file_get_contents($fileName);
-		return $this;
-	}
+    public function customData($customData)
+    {
+        $this->customData = $customData;
+
+        return $this;
+    }
+
+    public function text($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function textFromFile($fileName)
+    {
+        $this->text = file_get_contents($fileName);
+
+        return $this;
+    }
 }
 
 /**
@@ -98,53 +122,78 @@ class TextData{
  * @license  MIT
  * @link     http://www.yarakuzen.com
  */
-class RequestPayload{
+class RequestPayload
+{
+    /** @var string */
+    public $lcSrc;
 
-	public function lcSrc($source){
-		$this->lcSrc = $source;
-		return $this;
-	}
+    /** @var string */
+    public $lcTgt;
 
-	public function lcTgt($target){
-		$this->lcTgt = $target;
-		return $this;
-	}
+    /** @var bool */
+    public $machineTranslate;
 
-	public function machineTranslate($machineTranslate = true){
-		$this->machineTranslate = $machineTranslate != false;
-		return $this;
-	}
+    /** @var bool */
+    public $persist;
 
-    public function tier(Tier $tier){
+    /** @var int */
+    public $quote;
+
+    /** @var TextData[] */
+    public $texts = [];
+
+    /** @var string */
+    public $tier;
+
+    public function lcSrc($source)
+    {
+        $this->lcSrc = $source;
+        return $this;
+    }
+
+    public function lcTgt($target)
+    {
+        $this->lcTgt = $target;
+        return $this;
+    }
+
+    public function machineTranslate($machineTranslate = true)
+    {
+        $this->machineTranslate = $machineTranslate != false;
+        return $this;
+    }
+
+    public function tier(Tier $tier)
+    {
         $this->tier = $tier->toString();
         return $this;
     }
 
-    public function quote($quote = true){
-        $this->quote = $quote ? 1:0;
+    public function quote($quote = true)
+    {
+        $this->quote = $quote ? 1 : 0;
         return $this;
     }
 
-	public function persist($persist = true){
-		$this->persist = $persist;
-		return $this;
-	}
+    public function persist($persist = true)
+    {
+        $this->persist = $persist;
+        return $this;
+    }
 
-	public function addText(TextData $text){
-		if(!isset($this->texts))
-			$this->texts = array();
-		$this->texts[] = $text;
-		return $this;
-	}
+    public function addText(TextData $text)
+    {
+        $this->texts[] = $text;
+        return $this;
+    }
 }
-
 
 
 /**
  * Class DocBlock
  * Client
  *
- * The client is responsible for actually handling the call and signing the request. 
+ * The client is responsible for actually handling the call and signing the request.
  *
  * @category YarakuZen
  * @package  ClientLibrary
@@ -153,221 +202,266 @@ class RequestPayload{
  * @license  MIT
  * @link     http://www.yarakuzen.com
  */
-class Client{
+class Client
+{
+    const HTTP_POST = 1;
 
-	const HTTP_POST = 1;
+    /** @var string */
+    private $publicKey;
 
-	private $publicKey;
-	private $privateKey;
-
-
-	/////////////////////////////////////////////////////////
-	// Atributes that shouldn't actually be changed often //
-	///////////////////////////////////////////////////////
-
-	// default to Yaraku production env
-	protected $_url = "https://api.yarakuzen.com";
-
-	// default to 5 min
-	protected $_timeout = 300;
-
-	protected $_httpUser = null;
-	protected $_httpPass;
+    /** @var string  */
+    private $privateKey;
 
 
-	protected $_userAgent = "YarakuZen PHP Client v1.0";
-	protected $_referer = "";
+    /////////////////////////////////////////////////////////
+    // Attributes that shouldn't actually be changed often //
+    ///////////////////////////////////////////////////////
+
+    /**
+     * Default to Yaraku production env
+     * @var string
+     */
+    protected $url = "https://api.yarakuzen.com";
+
+    /**
+     * Default to 5 min
+     * @var int
+     */
+    protected $timeout = 300;
+
+    /** @var string | null */
+    protected $httpUser = null;
+
+    /** @var string */
+    protected $httpPass;
+
+    /** @var string */
+    protected $userAgent = "YarakuZen PHP Client v1.0";
+    
+    /** @var string */
+    protected $referer = "";
+
+    /**
+     * When using HTTPS disable the certificate validation
+     * @var bool
+     */
+    protected $insecure = false;
+
+    /**
+     * The charset for the request
+     * @var string
+     */
+    protected $charset = "UTF-8";
+
+    /** @var mixed */
+    protected $response;
+
+    /** @var mixed */
+    protected $status;
 
 
-	/**
-	 * When using HTTPS disable the certificate validation
-	 */
-	protected $_insecure = false;
+    /**
+     * Initializes the client using the given API Key and Secret generated at Yaraku.
+     *
+     * @param string $publicKey The API publicKey
+     * @param string $privateKey The API privateKey
+     */
+    public function __construct($publicKey, $privateKey)
+    {
+        $this->publicKey = $publicKey;
+        $this->privateKey = $privateKey;
+    }
 
+    ////////////////////////////
+    // Some Extra Parameters //
+    //////////////////////////
 
-	protected $_source = "jp";
-	protected $_target = "en";
+    /**
+     * Sets an alternative URL for calling the API.
+     *
+     * @param string $url The URL for YarakuZen.
+     * @return $this
+     */
+    public function url($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
 
+    /**
+     * Timeout for each request, in sec
+     * @param int $timeout
+     * @return $this
+     */
+    public function timeout($timeout)
+    {
+        $this->timeout = $timeout;
+        return $this;
+    }
 
-	/**
-	 * The charset for the request
-	 */
-	protected $_charset = "UTF-8";
+    /**
+     * Disables the certificate validation for HTTPS requests if parameter is unset or true.
+     * @param bool|true $insecure
+     * @return $this
+     */
+    public function insecure($insecure = true)
+    {
+        $this->insecure = !isset($insecure) || $insecure == true;
+        return $this;
+    }
 
+    /**
+     * Use HTTP authentication on top of publicKey/privateKey auth
+     *
+     * @param string $username The username for HTTP auth
+     * @param string $password The password for HTTP auth
+     * @return $this
+     */
+    public function auth($username, $password)
+    {
+        $this->httpUser = $username;
+        $this->httpPass = $password;
 
-	/**
-	 * Initializes the client using the given API Key and Secret generated at Yaraku.
-	 *
-	 * @param publicKey the API publicKey
-	 * @param privateKey the API privateKey
-	 */
-	function __construct($publicKey, $privateKey){
-		$this->publicKey = $publicKey;
-		$this->privateKey = $privateKey;
-	}
+        return $this;
+    }
 
-	////////////////////////////
-	// Some Extra Parameters //
-	//////////////////////////
+    /**
+     * Sets the userAgent informed to the server
+     *
+     * @param string $userAgent Should be a string that identifies your application to the server.
+     * @return $this
+     */
+    public function userAgent($userAgent)
+    {
+        $this->userAgent = $userAgent;
+        return $this;
+    }
 
-	/**
-	 * Sets an alternative URL for calling the API.
-	 *
-	 * @param url (optional) the URL for YarakuZen.
-	 * @return $this
-	 */
-	public function url($url){
-		$this->_url = $url;
-		return $this;
-	}
+    /**
+     * Set the referer informed to the server.
+     *
+     * @param string $referer The referer
+     * @return $this
+     */
+    public function referer($referer)
+    {
+        $this->referer = $referer;
+        return $this;
+    }
 
-	/**
-	 * Timeout for each request, in sec
-	 * @return $this
-	 */
-	public function timeout($timeout){
-		$this->_timeout = $timeout;
-		return $this;
-	}
+    /**
+     * Sets the charset for this request.
+     *
+     * @param string $charset The charset of the request
+     * @return $this
+     */
+    public function charset($charset)
+    {
+        $this->charset = $charset;
+        return $this;
+    }
 
-	/**
-	 * Disables the certificate validation for HTTPS requests if parameter is unset or true.
-	 * @return $this
-	 */
-	public function insecure($insecure=true){
-		$this->_insecure = !isset($insecure) || $insecure == true;
-		return $this;
-	}
+    //////////////////////
+    // Text API Method //
+    ////////////////////
 
-	/**
-	 * Use HTTP autentication on top of publicKey/privateKey auth
-	 *
-	 * @param $username the username for HTTP auth
-	 * @param $password the password for HTTP auth
-	 * @return $this
-	 */
-	public function auth($username, $password){
-		$this->_httpUser = $username;
-		$this->_httpPass = $password;
-		return $this;
-	}
+    /**
+     * @param mixed $payload
+     * @return mixed
+     * @throws Exception
+     */
+    public function callTexts($payload)
+    {
+        return $this->__callApi(Client::HTTP_POST, "texts", $payload);
+    }
 
-	/**
-	 * Sets the userAgent informed to the server
-	 *
-	 * @param $userAgent should be a string that identifies your application to the server.
-	 * @return $this
-	 */
-	public function userAgent($userAgent){
-		$this->_userAgent = $userAgent;
-		return $this;
-	}
+    ////////////////////
+    // Inner Working //
+    //////////////////
 
-	/**
-	 * Set the referer informed to the server.
-	 *
-	 * @param $referer the referer
-	 * @return $this
-	 */
-	public function referer($referer){
-		$this->_referer = $referer;
-		return $this;
-	}
+    /**
+     * Sign the given payload, returning it.
+     * @param $payload
+     * @return mixed
+     */
+    protected function __sign($payload)
+    {
+        $payload->publicKey = $this->publicKey;
+        // A Unix timestamp
+        $payload->timestamp = time();
+        // Create a sha1 hash
+        $payload->signature = hash_hmac('sha1', $payload->timestamp . $this->publicKey, $this->privateKey);
 
-	/**
-	 * Sets the charset for this request.
-	 *
-	 * @param $charset the charset of the request
-	 * @return $this
-	 */
-	public function charset($charset){
-		$this->_charset = $charset;
-		return $this;
-	}
+        return $payload;
+    }
 
-	//////////////////////
-	// Text API Method //
-	////////////////////
+    /**
+     * Return the given payload as a JSON encoded string of an object.
+     * Make sure it's actually signed before returning.
+     * @param mixed $payload
+     * @return string
+     */
+    protected function __preparePayload($payload)
+    {
+        if (is_object($payload)) {
+            $obj = clone $payload;
+        } elseif (is_array($payload)) {
+            $obj = (object)$payload;
+        } else {
+            $obj = (object)array();
+        }
 
-	public function callTexts($payload){
-		return $this->__callApi(Client::HTTP_POST, "texts", $payload);
-	}
+        $o = $this->__sign($obj);
 
-	////////////////////
-	// Inner Working //
-	//////////////////
+        return http_build_query($o);
+    }
 
-	/**
-	 * Sign the given payload, returning it.
-	 */
-	protected function __sign($payload){
-		$payload->publicKey = $this->publicKey;
-		$payload->timestamp = time(); // A Unix timestamp
-		$payload->signature = hash_hmac('sha1', $payload->timestamp.$this->publicKey, $this->privateKey); // Create a sha1 hash
+    /**
+     * Calls the API with the given payload returning the response in proper PHP object.
+     * @param int $httpMethod
+     * @param string $apiMethod
+     * @param mixed $payload
+     * @return mixed
+     * @throws Exception
+     */
+    protected function __callApi($httpMethod, $apiMethod, $payload)
+    {
+        $curl = curl_init();
+        $pl = $this->__preparePayload($payload);
 
-		return $payload;
-	}
+        curl_setopt($curl, CURLOPT_URL, $this->url . "/" . $apiMethod);
 
-	/**
-	 * Return the given payload as a JSON encoded string of an object.
-	 * Make sure it's actually signed before returning.
-	 */
-	protected function __preparePayload($payload){
-		if(is_object($payload))
-			$obj = clone $payload;
-		elseif(is_array($payload))
-			$obj = (object) $payload;
-		else
-			$obj = (object) array();
+        curl_setopt($curl, CURLOPT_TIMEOUT, $this->timeout);
 
+        // we need the response as a string
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-		$o = $this->__sign($obj);
-		return http_build_query($o);
-	}
+        if ($this->httpUser != null) {
+            curl_setopt($curl, CURLOPT_USERPWD, $this->httpUser . ':' . $this->httpPass);
+        }
 
-	/**
-	 * Calls the API with the given payload returning the response in proper PHP object.
-	 */
-	protected function __callApi($httpMethod, $apiMethod, $payload){
+        switch ($httpMethod) {
+            case Client::HTTP_POST:
+                curl_setopt($curl, CURLOPT_POST, true);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $pl);
+                break;
+            default:
+                throw new Exception("HTTP Method Not Supported");
+        }
 
-		$curl = curl_init();
-		$pl = $this->__preparePayload($payload);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, !$this->insecure);
+        curl_setopt($curl, CURLOPT_USERAGENT, $this->userAgent);
+        curl_setopt($curl, CURLOPT_REFERER, $this->referer);
 
+        $contentType = 'application/x-www-form-urlencoded;charset=' . $this->charset;
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-type: $contentType"]);
 
-		curl_setopt($curl, CURLOPT_URL, $this->_url."/".$apiMethod);
+        $this->response = curl_exec($curl);
+        $this->status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-		curl_setopt($curl, CURLOPT_TIMEOUT, $this->_timeout);
+        curl_close($curl);
 
-		// we need the response as a string
-		curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
-
-		if($this->_httpUser != null)
-			curl_setopt($curl, CURLOPT_USERPWD, $this->_httpUser.':'.$this->_httpPass);
-
-		switch($httpMethod){
-			case Client::HTTP_POST:
-				curl_setopt($curl,CURLOPT_POST, true);
-				curl_setopt($curl,CURLOPT_POSTFIELDS, $pl);
-				break;
-			default:
-				throw new Exception("HTTP Method Not Supported");
-		}
-
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, !$this->_insecure);
-		curl_setopt($curl, CURLOPT_USERAGENT, $this->_userAgent);
-		curl_setopt($curl, CURLOPT_REFERER, $this->_referer);
-
-
-		$contentType = 'application/x-www-form-urlencoded;charset='.$this->_charset;
-		curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-type: $contentType"]);
-
-		$this->_response = curl_exec($curl);
-		$this->_status = curl_getinfo($curl,CURLINFO_HTTP_CODE);
-
-		curl_close($curl); 
-
-		// TODO: treat the response
-		return json_decode($this->_response);
-	}
+        // TODO: treat the response
+        return json_decode($this->response);
+    }
 }
-?>
